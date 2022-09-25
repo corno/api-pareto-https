7006f85688d1d3e280c3d21782b10ef07d801a4c
corno <corno.astn.a@schraverus.com>
2022-09-25 11:53:49 +0200

    .

--- pub/src/interface/index.ts
+++ pub/src/interface/index.ts
@@ -3,4 +3,6 @@
 export * from "./functions/functions.p"
 export * from "./types/HTTPSError.p"
 export * from "./types/Path.p"
+
+export * from "./types/HostConfiguration.p"
 export * from "./interfaces/StreamConsumer.p"
